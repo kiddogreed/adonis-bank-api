54b64741a01754bee52d9b00dc80ec66633eaae5
john russelle domingo <johnrusselledomingo@gmail.com>
2022-12-12 19:00:18 +0800

    fully update to latest

--- start/routes.ts
+++ start/routes.ts
@@ -21,7 +21,7 @@ import Env from '@ioc:Adonis/Core/Env'
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/', ({ response }) => {
-  let message = { message: "BANK API(" + Env.get("NODE_ENV") + ")" };
+  let message = { message: "MBTC BANK API(" + Env.get("NODE_ENV") + ")" };
   response.json(message);
 })
 
@@ -29,20 +29,21 @@ Route.resource('info','infosController').apiOnly()
 
 
 
-Route.post('/auth', 'AuthController.login')
-Route.get('/auth/message', 'Classic/AuthController.message')
+Route.get('api/user','AuthController.index')
+Route.post('api/auth', 'AuthController.login')
+//Route.get('/auth/message', 'Classic/AuthController.message')
 
-Route.put('/account/email', 'Classic/MyAccountsController.email').middleware('auth:api')
-Route.put('/account/password', 'Classic/MyAccountsController.password').middleware('auth:api')
-Route.put('/account/password/reset', 'Classic/MyAccountsController.reset')
-Route.post('/account/password/reset-request', 'Classic/MyAccountsController.forgot')
-Route.delete('/account/delete', 'Classic/MyAccountsController.destroy').middleware('auth:api')
+// Route.put('/account/email', 'Classic/MyAccountsController.email').middleware('auth:api')
+// Route.put('/account/password', 'Classic/MyAccountsController.password').middleware('auth:api')
+// Route.put('/account/password/reset', 'Classic/MyAccountsController.reset')
+// Route.post('/account/password/reset-request', 'Classic/MyAccountsController.forgot')
+// Route.delete('/account/delete', 'Classic/MyAccountsController.destroy').middleware('auth:api')
 
-Route.post('/email/verify', 'Classic/EmailVerificationsController.verify')
-Route.post('/email/resend', 'Classic/EmailVerificationsController.resend')
+// Route.post('/email/verify', 'Classic/EmailVerificationsController.verify')
+// Route.post('/email/resend', 'Classic/EmailVerificationsController.resend')
 
-Route.get('/my/profile', 'Classic/MyProfilesController.show').middleware('auth:api')
-Route.put('/my/profile', 'Classic/MyProfilesController.update').middleware('auth:api')
+// Route.get('/my/profile', 'Classic/MyProfilesController.show').middleware('auth:api')
+// Route.put('/my/profile', 'Classic/MyProfilesController.update').middleware('auth:api')
 
 
-Route.post('/registration', 'RegistrationsController.store')
+Route.post('api/registration', 'RegistrationsController.store')
